6ffe4cd78ad3f6037b61d5d9b4665aabf77364ab
Peter Chapman <petechapman@protonmail.com>
2020-03-28 23:22:21 -0400

    node stuff

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           </FlexLayout>
         </GridItem>
         <GridItem col={{ xs: [1, new GridSpan(7)], md: [3, new GridSpan(3)] }} row={{ xs: 2 }}>
-          <Typography>This site is currently in progress. Feel Free to reach out at petechapman@protonmail.com</Typography>
+          <Typography>{"This site is currently in progress. Feel Free to reach out to me "}<a href="mailto:petechapman@protonmail.com">here</a></Typography>
         </GridItem>
       </GridLayout>
     </BreakpointProvider>
