5f6328ea7de538e0d2f96ee2fc00a0561f9f3841
Peter Chapman <petechapman@protonmail.com>
2020-04-25 20:39:52 -0400

    Blog: Ship Code Often ROUGH DRAFT

---
+++ src/Posts/FullPost/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Redirect, useParams } from "react-router";
+import { FlexLayout } from "../../FlexLayout";
+import { GridItem, GridLayout } from "../../Grid";
+import { Routes } from "../../Routes";
+import { Typography } from "../../Typography";
+import { findPostBySlug, PostModule, PostModuleKind, Posts } from "../posts";
+
+type FullPostProps = {};
+
+const renderModuleType = (post: PostModule) =>
+  post.kind === PostModuleKind.PARAGRAPH ? (
+    <Typography>{post.content}</Typography>
+  ) : null;
+
+export const FullPost: React.FC<FullPostProps> = () => {
+  const { slug = "" } = useParams();
+
+  const post = findPostBySlug(Posts, slug);
+
+  if (post.length === 0) {
+    return <Redirect to={Routes.POSTS} />;
+  }
+
+  const { title, date, content } = post[0];
+
+  return (
+    <GridLayout cols="subgrid">
+      <GridItem col={{ xs: [1, -1] }}>
+        <FlexLayout flexDirection="column">
+          <Typography variant="h4" style={{ margin: "0 0 0.25rem 0" }}>
+            {title}
+          </Typography>
+          <Typography variant="small" style={{ margin: "0 0 0.5rem 0" }}>
+            {date}
+          </Typography>
+        </FlexLayout>
+      </GridItem>
+      {content.map((post, i) => (
+        <GridItem key={`${post.kind}-${i}`} col={{ xs: [1, -1] }}>
+          {renderModuleType(post)}
+        </GridItem>
+      ))}
+    </GridLayout>
+  );
+};
